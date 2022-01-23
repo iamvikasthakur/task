d6ad38ad45d6a8c7b7a4a756d0620a414cd05b96
iamvikasthakur <vikasthakur11428@gmail.com>
2022-01-24 01:05:14 +0530

    Task Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const res = await fetch("https://randomuser.me/api/");
+    const data = await res.json();
+    console.log(data?.results?.[0]);
+    setData(data?.results?.[0]);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  if (!data) {
+    return <h1>Loading!!!</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={data?.picture?.large} alt="thumbnail" />
+      <h1>
+        Name : {`${data?.name?.title}. ${data?.name?.first} ${data?.name?.last}`}
+      </h1>
+      <h1>Gender: {`${data?.gender}`}</h1>
+      <h1>Age: {`${data?.dob?.age}`}</h1>
+      <h1>
+        Address:{" "}
+        {`${data?.location?.city} ${data?.location?.state} ${data?.location?.country}`}
+      </h1>
+      <h1>Email: {`${data?.email}`}</h1>
+      <h1>Phone: {`${data?.phone}`}</h1>
+      <button onClick={getData}>Update Profile</button>
     </div>
   );
 }
